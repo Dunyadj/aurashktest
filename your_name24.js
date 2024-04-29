bea5b4a621e3b2fb45a2894a8cf95e1c9274e682
aurashktest <165322301+aurashktest@users.noreply.github.com>
2024-04-29 10:20:39 -0400

    Add files via upload

--- your_name24.js
+++ your_name24.js
@@ -15,8 +15,9 @@ marker.bindPopup("<b>Edit Hello New York City!</b><br>This is a Leaflet map.").o
 
 
 
+
 // Load the GeoJSON line file
-fetch('https://aurashktest.github.io/aurashktest/akdaytimeroute.geojson')
+fetch('https://aurashktest.github.io/akdaytimeroute.geojson')
     .then(response => response.json())
     .then(geojson => {
         // Customize the style of the line
