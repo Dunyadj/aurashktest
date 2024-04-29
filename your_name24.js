3b92d1c4e05a8692cb9f8edcdddab11211fe14c2
aurashktest <165322301+aurashktest@users.noreply.github.com>
2024-04-29 10:54:19 -0400

    Add files via upload

--- your_name24.js
+++ your_name24.js
@@ -37,4 +37,22 @@ fetch('https://aurashktest.github.io/aurashktest/akdaytimeroute.geojson')
     });
 
 
+    // Load the GeoJSON line file
+fetch('https://aurashktest.github.io/aurashktest/akeveningroute.geojson')
+.then(response => response.json())
+.then(geojson => {
+    // Customize the style of the line
+    var lineStyle = {
+        color: 'red', // Change color as needed
+        weight: 5, // Change weight as needed
+        opacity: 0.7 // Change opacity as needed
+    };
 
+    // Add the GeoJSON line to the map
+    L.geoJSON(geojson, {
+        style: lineStyle
+    }).addTo(map);
+})
+.catch(error => {
+    console.error('Error loading GeoJSON file:', error);
+});
